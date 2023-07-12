caceef9346770e9190e39f812820c54a3c2f38a3
tunahan <ttezcan.1999@gmail.com>
2023-07-12 15:23:59 +0300

    expertise init

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
+import Expertise from "@/components/Expertise";
 import Home from "@/components/Home";
 
 const Page = () => (
   <section>
     <Home />
+    <Expertise />
   </section>
 );
 
---
+++ components/Expertise.jsx
@@ -0,0 +1,5 @@
+import "@/styles/expertise.scss";
+
+export default function Expertise() {
+  return <div className="expertise">Expertise</div>;
+}
