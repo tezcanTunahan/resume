92adcfb3f51b2b1425e5892e3162b7f4cb308c8f
tunahan <ttezcan.1999@gmail.com>
2023-07-19 09:53:32 +0300

    footer break

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,8 @@
 import "@/styles/footer.scss";
 export default function Footer() {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <h2>I don't know what to write here but this footer looks cool</h2>
+    </div>
+  );
 }
