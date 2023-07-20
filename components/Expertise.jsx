5c780deaa84cb783e25a9a3bbeb771b0e91e9c3a
tunahan <ttezcan.1999@gmail.com>
2023-07-20 16:08:57 +0300

    animation

--- components/Expertise.jsx
+++ components/Expertise.jsx
@@ -1,26 +1,50 @@
+"use client";
 import "@/styles/expertise.scss";
+import { motion, useInView, useAnimation } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 export default function Expertise() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  const mainControls = useAnimation();
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    } else {
+      mainControls.start("hidden");
+    }
+  }, [isInView]);
+
   return (
-    <div className="expertise">
-      <div className="expertise__top">
-        <h1 className="expertise__top__title">My Expertise</h1>
-      </div>
-      <div className="expertise__mid">
-        <h3 className="expertise__mid__subtitle">
-          HTML // CSS // Scss // Java // JavaScript // REACT// Node // Express
-          // <span style={{ color: "#adff2f" }}>NEXTJS</span> // Redux // Git //
-          MongoDB // NoSQL{" "}
-          <span style={{ color: "grey" }}>
-            and I'm willing to learn new tech stuff.
-          </span>
-        </h3>
-        <p className="expertise__mid__text">
-          I am a computer engineer who is in his senior year. I have a high
-          interest in frontend and web development. I am willing to learn and
-          try new things.
-        </p>
-      </div>
+    <div ref={ref} className="expertise">
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 1.5, delay: 0.5 }}
+      >
+        <div className="expertise__top">
+          <h1 className="expertise__top__title">My Expertise</h1>
+        </div>
+        <div className="expertise__mid">
+          <h3 className="expertise__mid__subtitle">
+            HTML // CSS // Scss // Java // JavaScript // REACT// Node // Express
+            // <span style={{ color: "#adff2f" }}>NEXTJS</span> // Redux // Git
+            // MongoDB // NoSQL{" "}
+            <span style={{ color: "grey" }}>
+              and I'm willing to learn new tech stuff.
+            </span>
+          </h3>
+          <p className="expertise__mid__text">
+            I am a computer engineer who is in his senior year. I have a high
+            interest in frontend and web development. I am willing to learn and
+            try new things.
+          </p>
+        </div>
+      </motion.div>
     </div>
   );
 }
--- components/Home.jsx
+++ components/Home.jsx
@@ -1,8 +1,22 @@
 "use client";
 import "@/styles/home.scss";
 import Button from "@/components/ui/Button";
+import { motion, useInView, useAnimation } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    } else {
+      mainControls.start("hidden");
+    }
+  }, [isInView]);
+
   const handleDownload = () => {
     const pdfFileName = "TunahanTezcan.pdf";
     const pdfPath = `/resume/${pdfFileName}`;
@@ -13,15 +27,24 @@ export default function Home() {
   };
 
   return (
-    <div className="home">
-      <div className="home__top">
+    <div ref={ref} className="home">
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 1.5, delay: 0.5 }}
+        className="home__top"
+      >
         <h1 className="home__top__title">Hi I'm Tunahan</h1>
         <h2 className="home__top__text">
           Software Engineer //{" "}
           <span className="text-animation">Frontend Developer</span>
         </h2>
         <Button text="Download Resume" onClick={handleDownload} />
-      </div>
+      </motion.div>
       <div className="home__bot"></div>
     </div>
   );
