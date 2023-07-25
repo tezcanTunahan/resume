631e56b205afec984dbe435a34f9a04892d5c97a
tunahan <ttezcan.1999@gmail.com>
2023-07-25 19:43:37 +0300

    card animation scroll

--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -1,10 +1,37 @@
+"use client";
 import "@/styles/ui/card.scss";
 import Image from "next/image";
+import { motion, useInView, useAnimation, useScroll } from "framer-motion";
+import { useEffect, useRef } from "react";
+
 export default function Card({ img, title, desc, tech, link }) {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    } else {
+      mainControls.start("hidden");
+    }
+  }, [isInView]);
+
   return (
-    <div className="card">
+    <div ref={ref} className="card">
       <div className="card--left">
-        <Image src={img} alt="" className="card--left__img" />
+        <motion.div
+          variants={{
+            hidden: { opacity: 0, x: -500 },
+            visible: { opacity: 1, x: 0 },
+          }}
+          initial="hidden"
+          animate={mainControls}
+          transition={{ duration: 1 }}
+          className="home__top"
+        >
+          <Image src={img} alt="" className="card--left__img" />
+        </motion.div>
       </div>
       <div className="card--right">
         <div className="card--right__title">
