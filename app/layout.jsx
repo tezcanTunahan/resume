b9b82e07435eafba5658409c38b7d53ca88a6969
tunahan <ttezcan.1999@gmail.com>
2023-09-30 21:06:07 +0300

    keywords

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,77 @@
-import Footer from "@/components/Footer";
-import Nav from "@/components/Nav";
-import "@/styles/global.scss";
+import Footer from '@/components/Footer';
+import Nav from '@/components/Nav';
+import '@/styles/global.scss';
 
 export const metadata = {
-  title: "Tunahan Tezcan - Software Engineer",
-  description: "Tunahan Tezcan - Software Engineer personal website",
+  title: 'Tunahan Tezcan - Software Engineer',
+  description: 'Tunahan Tezcan - Software Engineer personal website',
+  url: 'https://tunahantezcan.com',
+  image: '/images/og.png',
+  twitter: '@tunahantezcan',
+  keywords: [
+    'Tunahan Tezcan',
+    'Tunahan',
+    'Tezcan',
+    'Software Engineer',
+    'Software',
+    'Engineer',
+    'Full Stack',
+    'Full Stack Engineer',
+    'Full Stack Developer',
+    'Full Stack Software Engineer',
+    'Full Stack Software Developer',
+    'Full Stack Web Developer',
+    'Full Stack Web Engineer',
+    'Full Stack Web Software Engineer',
+    'Full Stack Web Software Developer',
+    'Full Stack Web',
+    'Full Stack Web Engineer',
+    'Full Stack Web Developer',
+    'Full Stack Web Software Engineer',
+    'Full Stack Web Software Developer',
+    'Full Stack Web',
+    'Full Stack Web Engineer',
+    'Full Stack Web Developer',
+    'Full Stack Web Software Engineer',
+    'Full Stack Web Software Developer',
+    'Full Stack Web',
+    'Full Stack Web Engineer',
+    'Full Stack Web Developer',
+    'Full Stack Web Software Engineer',
+    'Full Stack Web Software Developer',
+    'Full Stack Web',
+    'Full Stack Web Engineer',
+    'Full Stack Web Developer',
+    'Full Stack Web Software Engineer',
+    'Full Stack Web Software Developer',
+    'Full Stack Web',
+    'Full Stack Web Engineer',
+    'Full Stack Web Developer',
+    'Full Stack Web Software Engineer',
+    'Full Stack Web Software Developer',
+    'Full Stack Web',
+    'Full Stack Web Engineer',
+    'Full Stack Web Developer',
+    'Full Stack Web Software Engineer',
+    'Full Stack Web Software Developer',
+    'Full Stack Web',
+    'Full Stack Web Engineer',
+    'Full Stack Web Developer',
+    'Full Stack Web Software Engineer',
+    'Full Stack Web Software Developer',
+    'Full Stack Web',
+    'Full Stack Web Engineer',
+    'Full Stack Web Developer',
+    'Full Stack Web Software Engineer',
+    'Full Stack Web Software Developer',
+  ],
 };
 
 const RootLayout = ({ children }) => (
-  <html lang="en">
+  <html lang='en'>
     <body suppressHydrationWarning={true}>
       <Nav />
-      <main className="app">{children}</main>
+      <main className='app'>{children}</main>
       <Footer />
     </body>
   </html>
